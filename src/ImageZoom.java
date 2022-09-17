1f17fe35072905d1d060db13eebcef3e24e475c7
fmcshan <fmcshan@wpi.edu>
2022-09-17 18:14:47 -0400

    Add initial zooming

--- src/ImageZoom.java
+++ src/ImageZoom.java
@@ -1,11 +1,10 @@
-import java.awt.EventQueue;
+import java.awt.*;
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
-import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import java.awt.event.MouseWheelListener;
 import java.awt.event.MouseWheelEvent;
@@ -19,7 +18,7 @@ public class ImageZoom {
     public static final int SCALE_DEFAULT = 1;
 
     private JFrame frmImageZoomIn;
-    private static final String inputImage = "img.png";
+    private static final String inputImage = "fruit_0000.jpg";
     private JLabel label = null;
     private double zoom = 1.0;  // zoom factor
     private BufferedImage image = null;
@@ -63,23 +62,41 @@ public class ImageZoom {
         panel.addMouseWheelListener(new MouseWheelListener() {
             public void mouseWheelMoved(MouseWheelEvent e) {
                 int notches = e.getWheelRotation();
-                double temp = zoom - (notches * 0.2);
-                // minimum zoom factor is 1.0
-                temp = Math.max(temp, 1.0);
-                if (temp != zoom) {
-                    zoom = temp;
-                    //resizeImage();
+                if (notches > 0) {
+                    //System.out.println(notches);
+                    double temp = (notches * 0.2) + 1;
+                    //System.out.println(temp);
+                    resizeScale(temp, panel);
+                    //resizeScale(temp);
+                    //zoom = temp;
+                    panel.repaint();
+                    // minimum zoom factor is 1.0
+//                temp = Math.max(temp, 1.0);
+//                if (temp != zoom) {
+//                    zoom = temp;
+//                    resizeScale(zoom);
+//                    panel.repaint();
+//                }
                 }
-            }
+                }
         });
         scrollPane.setViewportView(panel);
     }
 
-    public BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
-        //Image resultingImage = originalImage.getScaledInstance(targetWidth, targetHeight, Image.SCALE_DEFAULT);
-        //BufferedImage outputImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-        //outputImage.getGraphics().drawImage((java.awt.Image) resultingImage, 0, 0, null);
-        return originalImage;
+    public void resizeScale (double scaleFactor, JPanel panel) {
+        int w = (int)Math.round(label.getIcon().getIconWidth() * scaleFactor);
+        int h = (int)Math.round(label.getIcon().getIconHeight() * scaleFactor);
+
+        BufferedImage resizedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(image, 0, 0, w, h, null);
+        graphics2D.dispose();
+        image = resizedImage;
+
+        label = new JLabel( new ImageIcon(image) );
+        panel.remove(0);
+        panel.add(label, BorderLayout.CENTER);
+
     }
 
 
